aaefde397df111f4323db55ad80fac2f42478e0b
vorSherer <tcpma2011@gmail.com>
2020-07-25 17:59:28 -0700

    Completed TreeNode class with passing tests (TDD).

--- dsna_py/data_structures/cc_15_tree/tree.py
+++ dsna_py/data_structures/cc_15_tree/tree.py
@@ -1,5 +1,8 @@
-class Node:
-    pass
+class TreeNode:
+    def __init__(self, value, left_child=None, right_child=None):
+        self.value = value
+        self.left_child = left_child
+        self.right_child = right_child
 
 
 class BinaryTree:
--- tests/data_structures/cc_15_tree/test_tree.py
+++ tests/data_structures/cc_15_tree/test_tree.py
@@ -1,12 +1,57 @@
 import pytest
 
 from dsna_py.data_structures.cc_15_tree.tree import (
-    Node,
-    # BinaryTree,
-    # BinarySearchTree,
+    TreeNode,
+    BinaryTree,
+    BinarySearchTree,
     )
 
 
-def test_Node_exists():
-    assert Node
+# Test connections to Module
+def test_TreeNode_exists():
+    assert TreeNode
+
+def test_bin_tree_exists():
+    assert BinaryTree
+
+def test_bin_srch_tree_exists():
+    assert BinarySearchTree
+
+
+# Test TreeNode
+def test_TreeNode_instantiation():
+    binary_node = TreeNode(7)
+    expected = 7
+    actual = binary_node.value
+    assert actual == expected
+
+
+# @pytest.mark.skip
+def test_Node_left_value(simple_tree):
+    binary_node = TreeNode(7)
+    expected = None
+    actual = binary_node.left_child
+    assert actual == expected
+
+
+# @pytest.mark.skip
+def test_Node_right_value(simple_tree):
+    binary_node = TreeNode(7)
+    expected = None
+    actual = binary_node.right_child
+    assert actual == expected
+
+
+@pytest.fixture
+def simple_tree():
+    #build a binary tree of three nodes
+    bt = BinaryTree()
+    root_node = TreeNode(7)
+    left_child = TreeNode(3)
+    right_child = TreeNode(1)
+
+
+@pytest.mark.skip
+def test_binary_tree_instantiates(simple_tree):
+    tree = simple_tree()
 
